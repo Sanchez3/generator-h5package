17c9b247efc4eab34000fe4833ecb80328b9f7ab
硫酸 <lsch001@163.com>
2017-09-07 15:32:26 +0800

    :wrench: Change config files

--- generators/app/templates/_webpack.config.js
+++ generators/app/templates/_webpack.config.js
@@ -1,40 +1,63 @@
-
 'use strict';
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
+var path = require('path');
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
-
-    entry: __dirname + '/src/entry.js', //唯一入口文件
+    entry: __dirname + "/src/assets/js/main.js",
     output: {
-        path: __dirname + '/build', //打包后的文件存放的地方
-        filename: 'bundle.js' //打包后输出文件的文件名
+        path: __dirname + "/dist",
+        filename: 'assets/js/[name]-[chunkhash].js'
     },
-
     module: {
-        loaders: [
-            { test: /\.js$/, loader: "jsx!babel", include: /src/},
-            { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css!postcss")},
-            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass")},
-            { test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
-        ]
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ["es2015"]
+                }
+            },
+            exclude: /node_modules/,
+            include: '/src/'
+        }, {
+            test: /\.css$/,
+            include: /src/,
+            use: ["style-loader", "css-loader"]
+        }, {
+            test: /(\.scss|\.sass)$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        }, {
+            test: /\.(html)$/,
+            use: {
+                loader: 'html-loader',
+                options: {
+                    minimize: true,
+                    removeComments: false,
+                    collapseWhitespace: false
+                }
+            }
+        }]
+
     },
-
-    postcss: [
-        require('autoprefixer')    //调用autoprefixer插件,css3自动补全
-    ],
-
     devServer: {
-        // contentBase: './src/views'  //本地服务器所加载的页面所在的目录
         port: 8888,
-        colors: true,  //终端中输出结果为彩色
-        historyApiFallback: true,  //不跳转
-        inline: true  //实时刷新
+        colors: true, //终端中输出结果为彩色
+        historyApiFallback: true, //不跳转
+        inline: true //实时刷新
     },
-
     plugins: [
-        new ExtractTextPlugin('main.css'),
-    ]
+        //清空dist
+        new CleanWebpackPlugin(["dist"], {
+            root: '',
+            verbose: true,
+            dry: false
+        }),
+        new HtmlWebpackPlugin({
 
-}
+
+        })
+
+    ]
+};
