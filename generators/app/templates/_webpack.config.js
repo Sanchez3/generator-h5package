429849a2e018dbf8c2de51f573c92b1b7c2b146b
硫酸 <lsch001@163.com>
2017-09-11 15:39:53 +0800

    :bug: :sparkles:  Import CSS and JS
    :bug: Css @charset cases bug
    
    :sparkles:  Import Js  (CommonJs/ES6)

--- generators/app/templates/_webpack.config.js
+++ generators/app/templates/_webpack.config.js
@@ -3,11 +3,15 @@
 var path = require('path');
 var CleanWebpackPlugin = require("clean-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var extractCSS = new ExtractTextPlugin('assets/css/[name]-one.css');
+var extractSASS = new ExtractTextPlugin('assets/css/[name]-two.css');
 
 module.exports = {
-    entry: __dirname + "/src/assets/js/main.js",
+    devtool: 'eval-source-map',
+    entry: path.resolve(__dirname, "src/assets/js/main.js"),
     output: {
-        path: __dirname + "/dist",
+        path: path.resolve(__dirname, 'dist'),
         filename: 'assets/js/[name].js',
         publicPath: 'localhost:8888/dist'
     },
@@ -25,10 +29,10 @@ module.exports = {
         }, {
             test: /\.css$/,
             include: /src/,
-            use: ["style-loader", "css-loader"]
+            use: extractCSS.extract(['css-loader'])
         }, {
             test: /(\.scss|\.sass)$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
+            use: extractSASS.extract(['css-loader', 'sass-loader'])
         }, {
             test: /\.html$/,
             use: {
@@ -42,14 +46,15 @@ module.exports = {
         }]
 
     },
-    
-    devtool: 'eval-source-map',
+
+
     devServer: {
-        contentBase: path.join(__dirname, "dist"), //本地服务器所加载的页面所在的目录
+        contentBase: path.resolve(__dirname, 'dist'), //本地服务器所加载的页面所在的目录
         port: 8888,
         colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转
-        inline: true //实时刷新
+        inline: true, //实时刷新
+        publicPath: '/'
     },
     plugins: [
         //清空dist
@@ -58,6 +63,8 @@ module.exports = {
             verbose: true,
             dry: false
         }),
+        extractCSS,
+        extractSASS,
         new HtmlWebpackPlugin({
             title: 'Custom template',
             chunks: ["main"],
