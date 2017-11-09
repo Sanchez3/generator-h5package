1777d9a4d15951464a14496ca28e7e9dd4962acb
硫酸 <lsch001@163.com>
2017-11-09 11:55:49 +0800

    :zap: Improving

--- generators/app/templates/src/assets/js/main.js
+++ generators/app/templates/src/assets/js/main.js
@@ -144,7 +144,11 @@ window.h5 = {
         return that;
     }
 };
-window.h5.cssInit().eventInit();
+
+window.onload = function() {
+    window.h5.cssInit().eventInit();
+};
+
 
 
 function showStats() {
