6c7ccc6724b7dfbc0eefe989a6096ae9e487734e
硫酸 <lsch001@163.com>
2018-01-08 17:24:49 +0800

    :zap: :ok_hand: Improving and Review Code

--- generators/app/templates/src/assets/js/main.js
+++ generators/app/templates/src/assets/js/main.js
@@ -33,7 +33,7 @@ window.h5 = {
         }
         return flag;
     },
-    rootResize2: function() {
+    rootResize: function() {
         //orientation landscape width=1334px
         var wFsize;
         //screen.width screen.height  bug !!!
@@ -49,53 +49,6 @@ window.h5 = {
         wFsize = wHeight / 13.34;
         document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
     },
-    rootResize1: function() {
-        //orientation landscape width=1334px
-        var that = this;
-        var Dpr = 1,
-            uAgent = window.navigator.userAgent;
-        var isIOS = uAgent.match(/iphone/i);
-        var isYIXIN = uAgent.match(/yixin/i);
-        var is2345 = uAgent.match(/Mb2345/i);
-        var ishaosou = uAgent.match(/mso_app/i);
-        var isSogou = uAgent.match(/sogoumobilebrowser/ig);
-        var isLiebao = uAgent.match(/liebaofast/i);
-        var isGnbr = uAgent.match(/GNBR/i);
-        var isWeixin = uAgent.match(/MicroMessenger/i);
-        var wFsize;
-        //screen.width screen.height  bug !!!
-        // var wWidth = (screen.width > 0) ? (window.innerWidth >= screen.width || window.innerWidth == 0) ? screen.width :
-        //     window.innerWidth : window.innerWidth;
-        // var wHeight = (screen.height > 0) ? (window.innerHeight >= screen.height || window.innerHeight == 0) ?
-        //     screen.height : window.innerHeight : window.innerHeight;
-        var wWidth = window.innerWidth;
-        var wHeight = window.innerHeight;
-        // if (isIOS) {
-        //     wWidth = screen.width;
-        //     wHeight = screen.height;
-        // }
-        if (wWidth > wHeight) {
-            wHeight = wWidth;
-        }
-        wFsize = wHeight / 13.34;
-        if (isYIXIN || is2345 || ishaosou || isSogou || isLiebao || isGnbr || isWeixin) { //YIXIN 和 2345 这里有个刚调用系统浏览器时候的bug，需要一点延迟来获取
-            setTimeout(function() {
-                wHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-                wWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-                if (wWidth > wHeight) {
-                    wHeight = wWidth;
-                }
-                wFsize = wHeight / 13.34;
-                // wFsize = wFsize > 32 ? wFsize : 32;
-                document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
-            }, 500);
-        } else {
-            document.getElementsByTagName('html')[0].style.fontSize = wFsize + 'px';
-        }
-
-        return that;
-
-    },
     eventInit: function() {
         var that = this;
         document.addEventListener('touchstart', function(e) {}, false);
@@ -106,22 +59,9 @@ window.h5 = {
     },
     cssInit: function() {
         var that = this;
-        /*
-        that.rootResize1();
-        window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
-            if (window.orientation == 90 || window.orientation == -90) {
-                //横屏
-                //_.renderShuping();
-                that.rootResize();
-            } else {
-                //竖屏
-                //_.closeShuping();
-            }
-        }, false);
-        */
         var noChangeCountToEnd = 100,
             noEndTimeout = 1000;
-        that.rootResize2();
+        that.rootResize();
         window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
             var interval,
                 timeout,
@@ -135,7 +75,7 @@ window.h5 = {
                 clearTimeout(timeout);
                 interval = null;
                 timeout = null;
-                that.rootResize2();
+                that.rootResize();
             };
             interval = setInterval(function() {
                 if (window.innerWidth === lastInnerWidth && window.innerHeight === lastInnerHeight) {
