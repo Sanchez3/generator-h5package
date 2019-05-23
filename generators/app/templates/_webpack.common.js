113c2e4bcd6529cd2d4e2bec727fa2441d6caea2
硫酸 <lsch001@163.com>
2019-05-23 13:41:50 +0800

    :sparkles: Add rsolve and expose-loader

--- generators/app/templates/_webpack.common.js
+++ generators/app/templates/_webpack.common.js
@@ -10,6 +10,13 @@ module.exports = {
     entry: {
         main: path.resolve(__dirname, "src/assets/js/main.js")
     },
+    // resolve: {
+    //     alias: {
+    //         pixi: path.resolve(__dirname, 'node_modules/phaser-ce/build/custom/pixi.min.js'),
+    //         phaser: path.resolve(__dirname, 'node_modules/phaser-ce/build/custom/phaser-split.min.js'),
+    //         p2: path.resolve(__dirname, 'node_modules/phaser-ce/build/custom/p2.js')
+    //     }
+    // },
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: "/",
@@ -31,70 +38,83 @@ module.exports = {
         }
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            },
-            exclude: /node_modules/,
-            include: '/src/'
-        }, {
-            test: /(\.css|\.scss|\.sass)$/,
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
+        rules: [
+            //exposing pixi, correct phaser and p2 into global scope (begin)
+            // {
+            //     test: /pixi\.min.js/,
+            //     loader: "expose-loader?PIXI"
+            // }, {
+            //     test: /phaser-split\.min.js/,
+            //     loader: "expose-loader?Phaser"
+            // }, {
+            //     test: /p2\.js/,
+            //     loader: "expose-loader?p2"
+            // }, 
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        publicPath: '../../'
+                        presets: ['@babel/preset-env']
                     }
                 },
-                { loader: 'css-loader' },
-                { loader: 'sass-loader' },
-                {
-                    loader: 'postcss-loader',
-                    options: { plugins: () => [require('autoprefixer')({ 'browsers': ['> 1%', 'last 2 versions'] })] }
+                exclude: /node_modules/,
+                include: '/src/'
+            }, {
+                test: /(\.css|\.scss|\.sass)$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../../'
+                        }
+                    },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: { plugins: () => [require('autoprefixer')({ 'browsers': ['> 1%', 'last 2 versions'] })] }
+                    }
+                ]
+            }, {
+                test: /\.(gif|jpg|png|ico)\??.*$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'assets/img/[name].[ext]'
+                    }
                 }
-            ]
-        }, {
-            test: /\.(gif|jpg|png|ico)\??.*$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'assets/img/[name].[ext]'
+            }, {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'assets/media/[name].[ext]'
+                    }
+                }
+            }, {
+                test: /\.(svg|woff|otf|ttf|eot)\??.*$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'assets/fonts/[name].[ext]'
+                    }
+                }
+            }, {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                        removeComments: false,
+                        collapseWhitespace: false,
+                        interpolate: 'require'
+                    }
                 }
             }
-        }, {
-            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'assets/media/[name].[ext]'
-                }
-            }
-        }, {
-            test: /\.(svg|woff|otf|ttf|eot)\??.*$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'assets/fonts/[name].[ext]'
-                }
-            }
-        }, {
-            test: /\.html$/,
-            use: {
-                loader: 'html-loader',
-                options: {
-                    minimize: true,
-                    removeComments: false,
-                    collapseWhitespace: false,
-                    interpolate: 'require'
-                }
-            }
-        }]
+        ]
     },
     plugins: [
         //清空dist
